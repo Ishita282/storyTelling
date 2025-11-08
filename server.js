d78cf8810693361ddb1408586d40edae46a920d3
Ishita282 <littleminiishita@gmail.com>
2025-11-08 17:52:57 +0530

    server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "docs")));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "docs", "index.html"));
 });
 
 const PORT = 3000;
